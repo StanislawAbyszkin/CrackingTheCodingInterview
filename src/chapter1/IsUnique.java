9406a933491c3493242a3557335318fe49fbe147
Stasiek Abyszkin <stasiek@Stasieks-MacBook-Pro.local>
2016-02-10 13:01:28 +0000

    Added solution wtih brute force with no data structures Added solution with hashMap implementation

--- src/chapter1/IsUnique.java
+++ src/chapter1/IsUnique.java
@@ -1,17 +1,18 @@
 package chapter1;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class IsUnique {
 
 	public static void main(String[] args) {
-		String input = "Hej";
+		String input = "Helllloa";
 
-		System.out.println(input + " has all unique characters: " + checkUniqueCharacters(input));
+		System.out.println(input + " has all unique characters: " + checkUniqueCharacters2(input));
 	}
 
 	/* BRUTE FORCE APPROACH AND USED ADDITIONAL DATA STRUCTURES -> BIG-O ( n^2 ) */
-	public static boolean checkUniqueCharacters(String s) {
+	public static boolean checkUniqueCharacters0(String s) {
 		ArrayList<Character> charUsed = new ArrayList<Character>();
 
 		for (int index = 0; index < s.length(); index++) {
@@ -24,4 +25,38 @@ public class IsUnique {
 		}
 		return true;
 	}
+	
+	/* BRUTE FORCE APPROACH AND NO ADDITIONAL DATA STRUCTURES -> BIG-O ( n^2 ) */
+	public static boolean checkUniqueCharacters1(String s) {
+		char[] charUsed = new char[s.length()];
+		int cnt = 0;
+		for (int index = 0; index < s.length(); index++) {
+			char currentLetter = s.charAt(index);
+			for (int j = 0; j < charUsed.length; j++) {
+				if (currentLetter == charUsed[j])
+					return false;
+			}
+			charUsed[cnt] = currentLetter;
+			cnt++;
+		}
+		return true;
+	}
+	
+	/* USE OF HASH-TABLE -> SHOULD RESULT IN BIG-O ( n * log n ) */
+	
+	public static boolean checkUniqueCharacters2(String s) {
+		HashMap<Character, Character> hashMap = new HashMap<>();
+		
+		for (int index = 0; index < s.length(); index++) {
+			char currentLetter = s.charAt(index);
+			if(hashMap.containsKey(currentLetter))
+				return false;
+			else
+			{
+				hashMap.put(currentLetter, null);
+			}
+		}
+		return true;
+	}
+
 }
